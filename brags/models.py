a7233367bbd315beea7378038d6d750f22123a18
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-11-06 18:31:38 -0300

    feat: adding extra models

--- brags/models.py
+++ brags/models.py
@@ -5,7 +5,6 @@ from django.core.validators import (MaxValueValidator, MinLengthValidator,
                                     MinValueValidator)
 from django.db import models
 
-
 class Category(models.Model):
     title = models.CharField(max_length=30, validators=[
                               MinLengthValidator(3)])
@@ -58,3 +57,30 @@ class BragTag(models.Model):
 
     def __str__(self):
         return f"Tag: {self.tag.title} - Brag: {self.brag.title}"
+    
+
+class Company(models.Model):
+    name = models.CharField(max_length=30,  validators=[
+                              MinLengthValidator(3)])
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Subscription(models.Model):
+    title = models.CharField(max_length=30,  validators=[
+                              MinLengthValidator(3)])
+    monthly_price = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=10)    
+
+    def __str__(self):
+        return f"{self.title} ($ {self.price})"
+
+class CompanySubscription():
+    company = models.ForeignKey('Company', on_delete=models.CASCADE)
+    subscription = models.ForeignKey('Subscription', on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created_at = models.DateField(auto_now=True)
