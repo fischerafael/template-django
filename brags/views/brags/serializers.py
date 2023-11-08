d4f16ddbcce6c2457ee4658a46a44bf407f12960
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-11-08 18:36:31 -0300

    feat: adjusted serializer

--- brags/views/brags/serializers.py
+++ brags/views/brags/serializers.py
@@ -5,13 +5,17 @@ from brags.models.brag.model import Brag
 
 class ListBragsSerializer(serializers.ModelSerializer):
     tags = serializers.SerializerMethodField()
+    category = serializers.SerializerMethodField()    
 
     class Meta:
         model = Brag
-        fields = ['id', 'title', 'duration', 'category', 'description', 'extra_link', 'is_public', 'status', 'tags']
+        exclude = ['is_public', 'user']
 
     def get_tags(self, obj):    
         titles = []
         for brag_tag in obj.tags.all():           
             titles.append(brag_tag.tag.title)   
         return titles
+    
+    def get_category(self, obj):
+        return obj.category.title
